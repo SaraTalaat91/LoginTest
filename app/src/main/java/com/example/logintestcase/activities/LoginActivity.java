7c5e8c7c729407bf1c2a3f042a8b998c4d95281b
SaraTMS <sara_talaat91@yahoo.com>
2019-08-24 23:05:20 +0200

    Handle login inputs

--- app/src/main/java/com/example/logintestcase/activities/LoginActivity.java
+++ app/src/main/java/com/example/logintestcase/activities/LoginActivity.java
@@ -4,24 +4,48 @@ import android.os.Build;
 import android.os.Bundle;
 
 import com.example.logintestcase.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.text.TextUtils;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.email_edit_text)
+    EditText emailEditText;
+    @BindView(R.id.password_edit_text)
+    EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+
+    @OnClick(R.id.login_button)
+    void loginUser(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        validateLogin(email, password);
+    }
+
+    private void validateLogin(String email, String password) {
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+
+        }else{
+            Toast.makeText(this, R.string.empty_fields_toast, Toast.LENGTH_SHORT).show();
         }
     }
 
