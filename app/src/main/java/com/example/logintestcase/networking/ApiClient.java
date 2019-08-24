4784ddf018f1ff855e92b43e6aea0b40e593cede
SaraTMS <sara_talaat91@yahoo.com>
2019-08-24 22:52:25 +0200

    Implement networking with retrofit Add necessary models

---
+++ app/src/main/java/com/example/logintestcase/networking/ApiClient.java
@@ -0,0 +1,21 @@
+package com.example.logintestcase.networking;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+ 
+    public static final String BASE_URL = "https://cpanels.shop/testTask/";
+    private static Retrofit retrofit = null;
+ 
+ 
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
