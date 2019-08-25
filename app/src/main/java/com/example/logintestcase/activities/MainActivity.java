d98837ac0d11cd3e760288f67177569b583067e9
SaraTMS <sara_talaat91@yahoo.com>
2019-08-25 02:37:14 +0200

    Implement signout

--- app/src/main/java/com/example/logintestcase/activities/MainActivity.java
+++ app/src/main/java/com/example/logintestcase/activities/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.logintestcase.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.logintestcase.R;
+import com.example.logintestcase.utilities.MySharedPref;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MySharedPref.setLoginModel(getApplicationContext(), null);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
